8b7a3fc25228b5d44cffe287a45fbc67e644f8a4
admin <admin@admins-MacBook-Air.local>
2026-02-01 22:58:23 +0530

    added product page and images

--- src/components/catalogue/productCatalogue.jsx
+++ src/components/catalogue/productCatalogue.jsx
@@ -1,31 +1,3 @@
-// import ProductCard from "./productCard";
-// import { catalogData } from "./products";
-
-// const ProductCatalog = () => {
-//   return (
-//     <div className="container py-5">
-//       <h2 className="text-center mb-5 fw-bold">
-//         Our Medical Product Catalogue
-//       </h2>
-
-//       {catalogData.map((section, index) => (
-//         <div key={index} className="mb-5">
-//           <h4 className="mb-4 border-bottom pb-2">
-//             {section.category}
-//           </h4>
-
-//           <div className="row">
-//             {section.products.map((product, idx) => (
-//               <ProductCard key={idx} product={product} />
-//             ))}
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default ProductCatalog;
 import { useState } from "react";
 import './index.css'
 import {categories} from './products'
@@ -39,7 +11,8 @@ export default function CatalogPage() {
   const hasSubcategories = !!activeCategory.subcategories;
 
   return (
-   <div className="container-fluid catalog-page">
+   <div className="container-fluid catalog-page" style={{padding:'20px'}}>
+    <hr/>
   <div className="row">
 
     {/* LEFT SIDEBAR */}
